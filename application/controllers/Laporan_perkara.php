a8ad48e2cb4e359b0edd371ab0c6c350e0418377
Jajang Kurniawan <j.kurniawan178@gmail.com>
2023-03-09 11:05:14 +0900

    perbaiki query LIPA 2 dan template baru

--- application/controllers/Laporan_perkara.php
+++ application/controllers/Laporan_perkara.php
@@ -57,6 +57,12 @@ class Laporan_perkara extends CI_Controller
           $hasil = $this->export_excel_lipa2($encoded, $jenis_laporan, $settingSIPP, $bulan, $tahun, $tanggal_laporan);
           echo json_encode($hasil);
           break;
+        case 'lipa_3':
+          $data = $this->laporan_model->getLIPA3($bulan, $tahun);
+          $encoded = json_encode($data);
+          $hasil = $this->export_excel_lipa3($encoded, $jenis_laporan, $settingSIPP, $bulan, $tahun, $tanggal_laporan);
+          echo json_encode($hasil);
+          break;
         default:
           # code...
           break;
@@ -71,9 +77,9 @@ class Laporan_perkara extends CI_Controller
     }
   }
 
-  public function export_excel_lipa2($data, $jenis_laporan, $settingSIPP, $bulan, $tahun, $tanggal_laporan)
+  protected function export_excel_lipa2($data, $jenis_laporan, $settingSIPP, $bulan, $tahun, $tanggal_laporan)
   {
-    if (!file_exists(FCPATH . "templates/" . $jenis_laporan . ".xls")) {
+    if (!file_exists(FCPATH . "new_templates/" . $jenis_laporan . ".xls")) {
       $response = [
         'kode' => '202',
         'data' => 'Template Belum Tersedia'
@@ -83,12 +89,12 @@ class Laporan_perkara extends CI_Controller
     }
 
     $objReader = PHPExcel_IOFactory::createReader('Excel5');
-    $objPHPExcel = $objReader->load(FCPATH . "templates/" . $jenis_laporan . ".xls");
+    $objPHPExcel = $objReader->load(FCPATH . "new_templates/" . $jenis_laporan . ".xls");
 
     $styleArray = array(
       'borders' => array(
         'allborders' => array(
-          'style' => PHPExcel_Style_Border::BORDER_THIN,
+          'style' => PHPExcel_Style_Border::BORDER_THIN
         ),
       ),
     );
@@ -122,6 +128,109 @@ class Laporan_perkara extends CI_Controller
     }
 
     $objPHPExcel->getActiveSheet()->getStyle('A9:L' . $row)->applyFromArray($styleArray);
+    $objPHPExcel->getActiveSheet()->getStyle('A9:L' . $row)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+
+    //tanda tangan 
+    $kolom_kpa = "C";
+    $kolom_pansek = "I";
+    $kota_pa = ucwords(strtolower(str_replace("PENGADILAN AGAMA ", "", str_replace("MAHKAMAH SYAR'IYAH ", "", $settingSIPP['NamaPN']))));
+
+    $KetuaPNNama = $settingSIPP['KetuaPNNama'];
+    $PanSekNama = $settingSIPP['PanSekNama'];
+    $KetuaPNNIP = $settingSIPP['KetuaPNNIP'];
+    $PanSekNIP = $settingSIPP['PanSekNIP'];
+    $row = $row + 3;
+    $row_awal_ttd = $row;
+    //fungsi tanda tangan
+
+    $objPHPExcel->getActiveSheet()
+      ->setCellValue($kolom_kpa . $row, "Mengetahui")
+      ->setCellValue($kolom_pansek . $row, $kota_pa . ", " . tgl_panjang_dari_mysql($tanggal_laporan))
+      ->getRowDimension($row)->setRowHeight(20);
+
+
+    $row++;
+    $objPHPExcel->getActiveSheet()
+      ->setCellValue($kolom_kpa . $row, "Ketua " . ucwords(strtolower($settingSIPP['NamaPN'])))
+      ->setCellValue($kolom_pansek . $row, "Panitera")
+      ->getRowDimension($row)->setRowHeight(20);
+    $row = $row + 5;
+    $objPHPExcel->getActiveSheet()
+      ->setCellValue($kolom_kpa . $row, $KetuaPNNama)
+      ->setCellValue($kolom_pansek . $row, $PanSekNama)
+      ->getRowDimension($row)->setRowHeight(20);
+    $row++;
+    $objPHPExcel->getActiveSheet()
+      ->setCellValue($kolom_kpa . $row, "NIP. " . $KetuaPNNIP)
+      ->setCellValue($kolom_pansek . $row, "NIP. " . $PanSekNIP)
+      ->getRowDimension($row)->setRowHeight(20);
+    $objPHPExcel->getActiveSheet()->getStyle($kolom_kpa . $row_awal_ttd . ':' . $kolom_pansek . $row)->getAlignment()->setWrapText(false);
+    $objPHPExcel->getActiveSheet()->getStyle($kolom_kpa . $row_awal_ttd . ':' . $kolom_pansek . $row)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_LEFT);
+
+    //fungsi tanda tangan 
+    //tanda tangan 
+    $objPHPExcel->getActiveSheet()->removeRow($baseRow, 1);
+
+    $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+    $namafile = $tahun . '_' . $bulan . '_' . $jenis_laporan . '.xlsx';
+    $file = FCPATH . 'hasil/' . $namafile;
+    $objWriter->save($file);
+    $response = [
+      'kode' => '200',
+      'data' => base_url() . 'hasil/' . $namafile
+    ];
+    return $response;
+  }
+
+  public function export_excel_lipa3($data, $jenis_laporan, $settingSIPP, $bulan, $tahun, $tanggal_laporan)
+  {
+    if (!file_exists(FCPATH . "templates/" . $jenis_laporan . ".xls")) {
+      $response = [
+        'kode' => '202',
+        'data' => 'Template Belum Tersedia'
+      ];
+      return $response;
+      exit;
+    }
+
+    $objReader = PHPExcel_IOFactory::createReader('Excel5');
+    $objPHPExcel = $objReader->load(FCPATH . "templates/" . $jenis_laporan . ".xls");
+
+    $styleArray = array(
+      'borders' => array(
+        'allborders' => array(
+          'style' => PHPExcel_Style_Border::BORDER_THIN,
+        ),
+      ),
+    );
+
+    $obj = json_decode($data, true);
+    $no = 1;
+    $baseRow = 9;
+
+    $objPHPExcel->getActiveSheet()->setCellValue('A2', "PADA " . $settingSIPP['NamaPN']);
+    $objPHPExcel->getActiveSheet()->setCellValue('A3', "BULAN " . strtoupper(pilihbulan($bulan)) . " " . $tahun);
+    foreach ($obj as $item) {
+      $row = $baseRow + $no;
+
+      $objPHPExcel->getActiveSheet()
+        ->setCellValue('A' . $row, $no)
+        ->setCellValue('B' . $row, $item['nomor_perkara_pn'])
+        ->setCellValue('C' . $row, $item['nomor_putusan_banding'])
+        ->setCellValue('D' . $row, tgl_dari_mysql($item['permohonan_kasasi']))
+        ->setCellValue('E' . $row, tgl_dari_mysql($item['penerimaan_memori_kasasi']))
+        ->setCellValue('F' . $row, tgl_dari_mysql($item['tidak_memenuhi_syarat']))
+        ->setCellValue('G' . $row, tgl_dari_mysql($item['pengiriman_berkas_kasasi']))
+        ->setCellValue('H' . $row, tgl_dari_mysql($item['putusan_kasasi']))
+        ->setCellValue('I' . $row, tgl_dari_mysql($item['penerimaan_berkas_kasasi']))
+        ->setCellValue('J' . $row, tgl_dari_mysql($item['pbt_putusan_p']) . chr(13) . tgl_dari_mysql($item['pbt_putusan_t']))
+        ->getRowDimension($row)->setRowHeight(46);
+
+      //$objPHPExcel->getActiveSheet()->insertNewRowAfter($row); 
+      $no++;
+    }
+
+    $objPHPExcel->getActiveSheet()->getStyle('A9:K' . $row)->applyFromArray($styleArray);
 
     //tanda tangan 
     $kolom_kpa = "C";
--- application/models/Laporan_model.php
+++ application/models/Laporan_model.php
@@ -30,60 +30,76 @@ class Laporan_model extends CI_Model
 
 
 	// ------------------------------------------------------------------------
-	public function index()
-	{
-		// 
-	}
 
 	public function getLIPA1($bulan, $tahun)
 	{
 	}
-
+	// ------------------------------------------------------------------------
 	public function getLIPA2($bulan, $tahun)
 	{
 		$sql = "
-					SELECT 
-					bdg.perkara_id
-					,perkara_penetapan.majelis_hakim_nama
-					, bdg.nomor_perkara_pn
-					, bdg.putusan_pn
-					, bdg.permohonan_banding
-					, (SELECT pemberitahuan_inzage FROM perkara_banding_detil WHERE perkara_id=bdg.perkara_id AND status_pihak_id=1 limit 1) as pbt_inzage_p
-					, (SELECT pemberitahuan_inzage FROM perkara_banding_detil WHERE perkara_id=bdg.perkara_id AND status_pihak_id=4 limit 1) as pbt_inzage_t
-					, bdg.pengiriman_berkas_banding
-					, bdg.putusan_banding
-					, bdg.penerimaan_kembali_berkas_banding 
-					, (SELECT pemberitahuan_putusan_banding FROM perkara_banding_detil WHERE perkara_id=bdg.perkara_id AND status_pihak_id=1 limit 1) as pbt_banding_p
-					, (SELECT pemberitahuan_putusan_banding FROM perkara_banding_detil WHERE perkara_id=bdg.perkara_id AND status_pihak_id=4 limit 1) as pbt_banding_t
-					FROM perkara_banding AS bdg
-					LEFT JOIN perkara_penetapan ON perkara_penetapan.perkara_id=bdg.perkara_id
-					WHERE LEFT(bdg.permohonan_banding,7)<'$tahun-$bulan' AND bdg.putusan_banding IS NULL OR LEFT(bdg.putusan_banding,7)>='$tahun-$bulan'
-
-					UNION 
-
-					SELECT 
-					bdg.perkara_id
-					,perkara_penetapan.majelis_hakim_nama
-					, bdg.nomor_perkara_pn
-					, bdg.putusan_pn
-					, bdg.permohonan_banding
-					, (SELECT pemberitahuan_inzage FROM perkara_banding_detil WHERE perkara_id=bdg.perkara_id AND status_pihak_id=1 limit 1) as pbt_inzage_p
-					, (SELECT pemberitahuan_inzage FROM perkara_banding_detil WHERE perkara_id=bdg.perkara_id AND status_pihak_id=4 limit 1) as pbt_inzage_t
-					, bdg.pengiriman_berkas_banding
-					, bdg.putusan_banding
-					, bdg.penerimaan_kembali_berkas_banding 
-					, (SELECT pemberitahuan_putusan_banding FROM perkara_banding_detil WHERE perkara_id=bdg.perkara_id AND status_pihak_id=1 limit 1) as pbt_banding_p
-					, (SELECT pemberitahuan_putusan_banding FROM perkara_banding_detil WHERE perkara_id=bdg.perkara_id AND status_pihak_id=4 limit 1) as pbt_banding_t
-					FROM perkara_banding AS bdg
-					LEFT JOIN perkara_penetapan ON perkara_penetapan.perkara_id=bdg.perkara_id
-					WHERE LEFT(bdg.permohonan_banding,7)='$tahun-$bulan' AND bdg.putusan_banding IS NULL OR LEFT(bdg.putusan_banding,7)>='$tahun-$bulan'
-				
+				SELECT 	bdg.perkara_id
+				,perkara_penetapan.majelis_hakim_nama
+				,bdg.nomor_perkara_pn
+				, bdg.putusan_pn
+				, bdg.permohonan_banding
+				, (SELECT pemberitahuan_inzage FROM perkara_banding_detil WHERE perkara_id=bdg.perkara_id AND status_pihak_id=1 limit 1) as pbt_inzage_p
+				, (SELECT pemberitahuan_inzage FROM perkara_banding_detil WHERE perkara_id=bdg.perkara_id AND status_pihak_id=4 limit 1) as pbt_inzage_t
+				, bdg.pengiriman_berkas_banding
+				, bdg.putusan_banding
+				, bdg.penerimaan_kembali_berkas_banding 
+				, (SELECT pemberitahuan_putusan_banding FROM perkara_banding_detil WHERE perkara_id=bdg.perkara_id AND status_pihak_id=1 limit 1) as pbt_banding_p
+				, (SELECT pemberitahuan_putusan_banding FROM perkara_banding_detil WHERE perkara_id=bdg.perkara_id AND status_pihak_id=4 limit 1) as pbt_banding_t
+				FROM perkara_banding AS bdg
+				LEFT JOIN perkara_penetapan ON perkara_penetapan.perkara_id=bdg.perkara_id
+				WHERE date_format( bdg.permohonan_banding,'%Y-%m')>='1900-01' and date_format( bdg.permohonan_banding,'%Y-%m')<='$tahun-$bulan' 
+				AND (bdg.putusan_banding IS NULL OR date_format(bdg.putusan_banding,'%Y-%m')>='$tahun-$bulan')
 				";
 		$hasil = $this->db->query($sql);
 		return $hasil->result();
 	}
 	// ------------------------------------------------------------------------
+	public function getLIPA3($bulan, $tahun)
+	{
+		$sql = "
+				SELECT 
+					kas.perkara_id
+					, kas.permohonan_kasasi
+					, kas.permohonan_kasasi
+					, kas.nomor_perkara_pn
+					, kas.penerimaan_memori_kasasi
+					, kas.tidak_memenuhi_syarat
+					, kas.pengiriman_berkas_kasasi
+					, kas.putusan_kasasi
+					, kas.penerimaan_berkas_kasasi
+					, (SELECT pemberitahuan_putusan_kasasi FROM perkara_kasasi_detil WHERE perkara_id=kas.perkara_id AND status_pihak_id=1 limit 1) AS pbt_putusan_p
+					, (SELECT pemberitahuan_putusan_kasasi FROM perkara_kasasi_detil WHERE perkara_id=kas.perkara_id AND status_pihak_id=4 limit 1) AS pbt_putusan_t
+					, bdg.nomor_putusan_banding
+					FROM perkara_kasasi AS kas
+					LEFT JOIN perkara_banding AS bdg ON bdg.perkara_id=kas.perkara_id
+					WHERE LEFT(kas.permohonan_kasasi,7)< '$tahun-$bulan' AND kas.putusan_kasasi IS NULL OR LEFT(kas.putusan_kasasi,7)>='$tahun-$bulan'
 
+					UNION
+					SELECT 
+					kas.perkara_id
+					, kas.permohonan_kasasi
+					, kas.permohonan_kasasi
+					, kas.nomor_perkara_pn
+					, kas.penerimaan_memori_kasasi
+					, kas.tidak_memenuhi_syarat
+					, kas.pengiriman_berkas_kasasi
+					, kas.putusan_kasasi
+					, kas.penerimaan_berkas_kasasi
+					, (SELECT pemberitahuan_putusan_kasasi FROM perkara_kasasi_detil WHERE perkara_id=kas.perkara_id AND status_pihak_id=1 limit 1) AS pbt_putusan_p
+					, (SELECT pemberitahuan_putusan_kasasi FROM perkara_kasasi_detil WHERE perkara_id=kas.perkara_id AND status_pihak_id=4 limit 1) AS pbt_putusan_t
+					, bdg.nomor_putusan_banding
+					FROM perkara_kasasi AS kas
+					LEFT JOIN perkara_banding AS bdg ON bdg.perkara_id=kas.perkara_id
+					WHERE LEFT(kas.permohonan_kasasi,7)='$tahun-$bulan' AND kas.putusan_kasasi IS NULL OR LEFT(kas.putusan_kasasi,7)>='$tahun-$bulan'
+				";
+		$hasil = $this->db->query($sql);
+		return $hasil->result();
+	}
 }
 
 /* End of file Laporan_model.php */
