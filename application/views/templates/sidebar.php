992dcf84e831a9910268a5f9c40bb9aec362bf52
Jajang Kurniawan <j.kurniawan178@gmail.com>
2023-03-20 11:22:39 +0900

    fix lipa 6 finished

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -6,11 +6,11 @@
       </li>
       <li><a><i class="fa fa-edit"></i> Input Laporan Manual <span class="fa fa-chevron-down"></span></a>
         <ul class="nav child_menu">
-          <li><a href="form_advanced.html">Laporan Keuangan Perkara Konsignasi (LIPA.7C)</a></li>
-          <li><a href="<?php echo site_url() ?>input_iwadl">Laporan Pertanggungjawaban Uang Iwadl (LIPA.11) </a></li>
+          <!-- <li><a href="form_advanced.html">Laporan Keuangan Perkara Konsignasi (LIPA.7C)</a></li> -->
+          <!-- <li><a href="<?php echo site_url() ?>input_iwadl">Laporan Pertanggungjawaban Uang Iwadl (LIPA.11) </a></li> -->
           <li><a href="<?php echo site_url() ?>sidkel">Laporan Pelaksanaan Sidang Keliling (LIPA.14)</a></li>
-          <li><a href="form_validation.html">Laporan Pelaksanaan Prodeo (LIPA.15)</a></li>
-          <li><a href="form_wizards.html">Laporan Pelaksanaan Posbakum (LIPA.16)</a></li>
+          <!-- <li><a href="form_validation.html">Laporan Pelaksanaan Prodeo (LIPA.15)</a></li> -->
+          <!-- <li><a href="form_wizards.html">Laporan Pelaksanaan Posbakum (LIPA.16)</a></li> -->
         </ul>
       </li>
       <li><a><i class="fa fa-desktop"></i> Laporan <span class="fa fa-chevron-down"></span></a>
@@ -24,13 +24,13 @@
             <ul class="nav child_menu" style="display: none;">
               <li class="sub_menu"><a href="<?php echo site_url() ?>pagu_14">Pagu Anggaran LIPA 14</a>
               </li>
-              <li><a href="#level2_1">Pagu Anggaran LIPA 15</a>
+              <!-- <li><a href="#level2_1">Pagu Anggaran LIPA 15</a>
               </li>
               <li><a href="#level2_2">Pagu Anggaran LIPA 16</a>
-              </li>
+              </li> -->
             </ul>
           </li>
-          <li><a href="tables_dynamic.html">Saldo Awal Keuangan Perkara</a></li>
+          <!-- <li><a href="tables_dynamic.html">Saldo Awal Keuangan Perkara</a></li> -->
         </ul>
       </li>
   </div>
