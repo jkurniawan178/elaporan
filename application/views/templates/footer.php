6ce2cd448c14664286b9d6c8ccfd9c474f37700a
Jajang Kurniawan <j.kurniawan178@gmail.com>
2023-08-02 11:34:35 +0900

    delete library yang tidak dibutuhkan

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -13,39 +13,8 @@
 <script src="<?php echo base_url() ?>resources/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap -->
 <script src="<?php echo base_url() ?>resources/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<!-- FastClick -->
-<!-- <script src="<?php echo base_url() ?>resources/fastclick/lib/fastclick.js"></script> -->
-<!-- NProgress -->
-<!-- <script src="<?php echo base_url() ?>resources/nprogress/nprogress.js"></script> -->
 <!-- Chart.js -->
 <!-- <script src="<?php echo base_url() ?>resources/Chart.js/dist/Chart.min.js"></script> -->
-<!-- gauge.js -->
-<!-- <script src="<?php echo base_url() ?>resources/gauge.js/dist/gauge.min.js"></script> -->
-<!-- bootstrap-progressbar -->
-<!-- <script src="<?php echo base_url() ?>resources/bootstrap-progressbar/bootstrap-progressbar.min.js"></script> -->
-<!-- iCheck -->
-<!-- <script src="<?php echo base_url() ?>resources/iCheck/icheck.min.js"></script> -->
-<!-- Skycons -->
-<!-- <script src="<?php echo base_url() ?>resources/skycons/skycons.js"></script> -->
-<!-- Flot -->
-<!-- <script src="<?php echo base_url() ?>resources/Flot/jquery.flot.js"></script> -->
-<!-- <script src="<?php echo base_url() ?>resources/Flot/jquery.flot.pie.js"></script> -->
-<!-- <script src="<?php echo base_url() ?>resources/Flot/jquery.flot.time.js"></script> -->
-<!-- <script src="<?php echo base_url() ?>resources/Flot/jquery.flot.stack.js"></script> -->
-<!-- <script src="<?php echo base_url() ?>resources/Flot/jquery.flot.resize.js"></script> -->
-<!-- Flot plugins -->
-<!-- <script src="<?php echo base_url() ?>resources/flot.orderbars/js/jquery.flot.orderBars.js"></script> -->
-<!-- <script src="<?php echo base_url() ?>resources/flot-spline/js/jquery.flot.spline.min.js"></script> -->
-<!-- <script src="<?php echo base_url() ?>resources/flot.curvedlines/curvedLines.js"></script> -->
-<!-- DateJS -->
-<!-- <script src="<?php echo base_url() ?>resources/DateJS/build/date.js"></script> -->
-<!-- JQVMap -->
-<!-- <script src="<?php echo base_url() ?>resources/jqvmap/dist/jquery.vmap.js"></script> -->
-<!-- <script src="<?php echo base_url() ?>resources/jqvmap/dist/maps/jquery.vmap.world.js"></script> -->
-<!-- <script src="<?php echo base_url() ?>resources/jqvmap/examples/js/jquery.vmap.sampledata.js"></script> -->
-<!-- bootstrap-daterangepicker -->
-<script src="<?php echo base_url() ?>resources/moment/min/moment.min.js"></script>
-<script src="<?php echo base_url() ?>resources/bootstrap-daterangepicker/daterangepicker.js"></script>
 
 <!-- Page level plugins -->
 <script src="<?php echo base_url() ?>resources/datatables/jquery.dataTables.min.js"></script>
