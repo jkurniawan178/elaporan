6c277b9ec31e3dc0b328bde825868e59a7f37733
Jajang Kurniawan <j.kurniawan178@gmail.com>
2023-08-17 22:24:12 +0900

    add halaman dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -28,7 +28,27 @@ class Dashboard extends CI_Controller
 	public function index()
 	{
 		$data['contents'] = 'v_dashboard';
+		$data['dashboard'] = $this->getDashboard();
 		$data['settings'] = $this->config_library->get_config_SIPP();
 		$this->load->view('templates/index', $data);
+	}
+
+	public function getDashboard()
+	{
+		$sql = "SELECT C.sisatahunlalu, C.masuk, C.minutasi, C.sisaperkara, C.putus, C.belumminutasi, C.ecourt,
+		ROUND(SUM(C.minutasi)*100/(SUM(C.masuk)+SUM(C.sisatahunlalu)),2) AS kinerjaPN,
+		round((C.ecourt/C.masuk)*100,2) as rasio_ecourt
+		FROM (SELECT
+		SUM(CASE WHEN YEAR(A.tanggal_pendaftaran)<=YEAR(NOW())-1 AND (YEAR(B.tanggal_minutasi)>=YEAR(NOW()) OR (B.tanggal_minutasi IS NULL OR B.tanggal_minutasi='')) THEN 1 ELSE 0 END) AS sisatahunlalu,
+		SUM(CASE WHEN YEAR(A.tanggal_pendaftaran)=YEAR(NOW()) THEN 1 ELSE 0 END) AS masuk,
+		SUM(CASE WHEN YEAR(A.tanggal_pendaftaran)<=YEAR(NOW()) AND YEAR(B.tanggal_minutasi)=YEAR(NOW()) THEN 1 ELSE 0 END) AS minutasi,
+		SUM(CASE WHEN YEAR(A.tanggal_pendaftaran)<=YEAR(NOW()) AND (B.tanggal_minutasi)IS NULL THEN 1 ELSE 0 END) AS sisaperkara,
+		SUM(CASE WHEN YEAR(A.tanggal_pendaftaran)<=YEAR(NOW()) AND YEAR(B.tanggal_putusan)=YEAR(NOW()) THEN 1 ELSE 0 END) AS putus,
+		SUM(CASE WHEN YEAR(A.tanggal_pendaftaran)<=YEAR(NOW()) AND (YEAR(B.tanggal_putusan)<=YEAR(NOW()) AND (B.tanggal_minutasi IS NULL OR B.tanggal_minutasi='')) THEN 1 ELSE 0 END) AS belumminutasi,
+		(SELECT SUM(CASE WHEN efiling_id THEN 1 ELSE 0 END) FROM `perkara_efiling` WHERE nomor_perkara IS NOT NULL AND YEAR(tgl_pendaftaran_perkara) = year(now())) as ecourt
+		FROM perkara AS A LEFT JOIN perkara_putusan AS B ON A.perkara_id=B.perkara_id WHERE A.alur_perkara_id <> 114) AS C
+		";
+		$hasil = $this->db->query($sql);
+		return $hasil->row();
 	}
 }
--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -1,3 +1,102 @@
 <div class="right_col" role="main">
-  <h2>Dahsboard Aplikasi E-Laporan Perkara</h2>
+  <h2 class="mb-4">Penanganan Perkara Tingkat Pertama Tahun 2023</h2>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-xl-3">
+        <div class="card bg-c-blue order-card">
+          <div class="card-block">
+            <h6 class="m-b-20">Rasio Penanganan Perkara</h6>
+            <h2 class="text-right"><i class="fa fa-pie-chart f-left"></i><span class="persen"><?php echo $dashboard->kinerjaPN ?>%</span></h2>
+            <p class="m-b-0">Masuk Tahun ini<span class="f-right"><?php echo $dashboard->masuk ?></span></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-xl-3">
+        <div class="card bg-c-green order-card">
+          <div class="card-block">
+            <h6 class="m-b-20">Belum Minutasi</h6>
+            <h2 class="text-right"><i class="fa fa-minus-square f-left"></i><span class="persen"><?php echo $dashboard->belumminutasi ?></span></h2>
+            <p class="m-b-0">Minutasi Tahun Ini<span class="f-right"><?php echo $dashboard->minutasi ?></span></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-xl-3">
+        <div class="card bg-c-pink order-card">
+          <div class="card-block">
+            <h6 class="m-b-20">Sisa Perkara</h6>
+            <h2 class="text-right"><i class="fa fa-credit-card f-left"></i><span class="persen"><?php echo $dashboard->sisaperkara ?></span></h2>
+            <p class="m-b-0">Putus Tahun Ini<span class="f-right"><?php echo $dashboard->putus ?></span></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-xl-3">
+        <div class="card bg-c-yellow order-card">
+          <div class="card-block">
+            <h6 class="m-b-20">Rasio Perkara E-Court</h6>
+            <h2 class="text-right"><i class="fa fa-globe f-left"></i><span class="persen"><?php echo $dashboard->rasio_ecourt ?>%</span></h2>
+            <p class="m-b-0">E-Court Diterima Tahun Ini<span class="f-right"><?php echo $dashboard->ecourt ?></span></p>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
 </div>
+
+<style>
+  .order-card {
+    color: #fff;
+  }
+
+  .bg-c-blue {
+    background: #0089CB;
+  }
+
+  .bg-c-green {
+    background: #70AA51;
+  }
+
+  .bg-c-yellow {
+    background: #DEB887;
+  }
+
+  .bg-c-pink {
+    background: #D74642;
+  }
+
+
+  .card {
+    border-radius: 5px;
+    -webkit-box-shadow: 0 1px 2.94px 0.06px rgba(4, 26, 55, 0.16);
+    box-shadow: 0 1px 2.94px 0.06px rgba(4, 26, 55, 0.16);
+    border: none;
+    margin-bottom: 30px;
+    -webkit-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+  }
+
+  .card .card-block {
+    padding: 25px;
+  }
+
+  .order-card i {
+    font-size: 26px;
+  }
+
+  .f-left {
+    float: left;
+  }
+
+  .f-right {
+    float: right;
+  }
+
+  .persen {
+    font-size: 2rem;
+    font-weight: 600;
+  }
+</style>
