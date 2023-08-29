d1c4a72450f4d32bfc8d25b948a719c7a0a0dbad
Jajang Kurniawan <j.kurniawan178@gmail.com>
2023-08-29 15:48:23 +0900

    add favicon

--- application/views/login/v_login1.php
+++ application/views/login/v_login1.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Laporan - Login</title>
-    <link rel="icon" href="images/favicon.ico" type="image/ico">
+    <link rel="icon" href="<?php echo base_url() ?>resources/images/favicon.ico" type="image/ico">
     <link href="<?php echo base_url() ?>resources/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
     <link href="<?php echo base_url() ?>resources/font-awesome/css/font-awesome.min.css" rel="stylesheet">
