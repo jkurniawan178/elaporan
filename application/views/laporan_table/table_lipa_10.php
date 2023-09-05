39fc8cc785fcb29b28da44cdac027737ce0315db
Jajang Kurniawan <j.kurniawan178@gmail.com>
2023-09-05 17:44:16 +0900

    add view table to laporan lipa 10

---
+++ application/views/laporan_table/table_lipa_10.php
@@ -0,0 +1,82 @@
+<div class="table-responsive">
+    <table class="text-center table table-striped table-bordered" id="table_lipa10">
+        <thead class="bg-success text-dark">
+            <tr>
+                <th class="align-middle p-2" rowspan="2">No.</th>
+                <th class="align-middle p-2" colspan="13">Penyebab Terjadinya Perceraian</th>
+                <th class="align-middle p-2" rowspan="2">Jumlah</th>
+            </tr>
+            <tr>
+                <th scope="col" class="align-middle p-2">Zina</th>
+                <th scope="col" class="align-middle p-2">Mabuk</th>
+                <th scope="col" class="align-middle p-2">Madat</th>
+                <th scope="col" class="align-middle p-2">Judi</th>
+                <th scope="col" class="align-middle p-2">Meninggalkan Salah Satu Pihak</th>
+                <th scope="col" class="align-middle p-2">Dihukum Penjara</th>
+                <th scope="col" class="align-middle p-2">Poligami</th>
+                <th scope="col" class="align-middle p-2">KDRT</th>
+                <th scope="col" class="align-middle p-2">Cacat Badan</th>
+                <th scope="col" class="align-middle p-2">Perselisihan Dan Pertengkaran Terus Menerus</th>
+                <th scope="col" class="align-middle p-2">Kawin Paksa</th>
+                <th scope="col" class="align-middle p-2">Murtad</th>
+                <th scope="col" class="align-middle p-2">Ekonomi</th>
+            </tr>
+            <tr class="bg-warning">
+                <th scope="col" class="align-middle py-0">1</th>
+                <th scope="col" class="align-middle py-0">2</th>
+                <th scope="col" class="align-middle py-0">3</th>
+                <th scope="col" class="align-middle py-0">4</th>
+                <th scope="col" class="align-middle py-0">5</th>
+                <th scope="col" class="align-middle py-0">6</th>
+                <th scope="col" class="align-middle py-0">7</th>
+                <th scope="col" class="align-middle py-0">8</th>
+                <th scope="col" class="align-middle py-0">9</th>
+                <th scope="col" class="align-middle py-0">10</th>
+                <th scope="col" class="align-middle py-0">11</th>
+                <th scope="col" class="align-middle py-0">12</th>
+                <th scope="col" class="align-middle py-0">13</th>
+                <th scope="col" class="align-middle py-0">14</th>
+                <th scope="col" class="align-middle py-0">15</th>
+            </tr>
+        </thead>
+        <tbody id="show_data"></tbody>
+    </table>
+</div>
+<script>
+    function generateTableRows(data) {
+        const table = $('#table_lipa10').DataTable({
+            order: [
+                [0, 'asc']
+            ],
+            "bFilter": false,
+            columnDefs: [{
+                    className: "align-middle",
+                    targets: ["_all"]
+                } // Apply class to specific columns
+            ],
+        });
+        table.clear();
+        // Loop through the received data and create rows for the table
+        for (let i = 0; i < data.length; i++) {
+
+            table.row.add([
+                i + 1,
+                addThousandSeparator(data[i].zina),
+                addThousandSeparator(data[i].mabuk),
+                addThousandSeparator(data[i].madat),
+                addThousandSeparator(data[i].judi),
+                addThousandSeparator(data[i].meninggalkan),
+                addThousandSeparator(data[i].dihukum),
+                addThousandSeparator(data[i].poligami),
+                addThousandSeparator(data[i].kdrt),
+                addThousandSeparator(data[i].cacat),
+                addThousandSeparator(data[i].perselisihan),
+                addThousandSeparator(data[i].kawin_paksa),
+                addThousandSeparator(data[i].murtad),
+                addThousandSeparator(data[i].ekonomi),
+                addThousandSeparator(data[i].jumlah)
+            ])
+        }
+        table.draw();
+    }
+</script>
