2d54aab497b37eff57131abc5e6941df2aed6b4b
Jajang Kurniawan <j.kurniawan178@gmail.com>
2025-02-04 16:05:06 +0900

    change baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "http://" . $_SERVER['HTTP_HOST'] . "/elaporan/";
+$base_url = "http" . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "s" : "") . "://" . $_SERVER['HTTP_HOST'] . str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $base_url;
 
 /*
 |--------------------------------------------------------------------------
